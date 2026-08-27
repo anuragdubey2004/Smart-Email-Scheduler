cf38c96941fbb93b12c61129a9d1262fead80a41
anuragdubey2004 <anuragdubey635@gmail.com>
2026-08-27 09:37:33 +0530

    Resolved CORS issue

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], # Your Vite port
+    allow_origins=["https://smart-email-scheduler-eight.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
